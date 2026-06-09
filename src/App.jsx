10a8e1db40b0629ed83109030f7843ce2e4db086
Nabil <nabilehsanul@gmail.com>
2026-06-09 22:49:53 +0600

    fix: remove unused commented imports causing build crash

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-// import PlayWithButton from "./components/PlayWithButton";
 import CartApp from "./cart/CartApp.jsx";
-import HookForm from "./components/hookForm.jsx";
-// import Person from "./components/Person.jsx"
+
 function App() {
   return (
     <>
-      {/* <Alert color="info" />
-      <Button handleClick={handleClick} color="info">
-        My Button
-      </Button> */}
-      {/* <PlayWithButton /> */}
-      {/* <Person></Person> */}
-      {/* <HookForm/> */}
-      <CartApp/>
-     
+    <CartApp/>
     </>
   );
 }
